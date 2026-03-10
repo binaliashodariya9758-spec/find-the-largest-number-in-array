768e6ba5e07fd5a674a43b16452d88f0c0f5204a
binaliashodariya9758-spec <binaliashodariya9758@gmail.com>
2026-03-10 15:21:36 +0530

    Add files via upload

---
+++ findthelargestelementinarray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter n:";
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int largest = arr[0];
+
+    for(int i = 1; i < n; i++)
+    {
+        if(arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+
+    cout << "Largest number is: " << largest;
+
+    return 0;
+}
